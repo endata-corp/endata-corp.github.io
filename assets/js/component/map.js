5db37aa4499dc12321d12481b046a1018fd50fc6
Ricardo Rossi <rrossi@endata.com>
2016-04-20 20:40:39 -0500

    chopped down the sections for now

--- assets/js/component/map.js
+++ assets/js/component/map.js
@@ -1,15 +1,15 @@
 $(function(){
     $(window).load(function(){
         "use strict";
-        
+
         // set your google maps parameters
-        var $latitude = -6.193948, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-            $longitude = 106.849184,
-            $map_zoom = 16; /* ZOOM SETTING */
+        var $latitude = 32.948333, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+            $longitude = -96.729852,
+            $map_zoom = 10; /* ZOOM SETTING */
 
         // google map custom marker icon
         var $marker_url = 'assets/img/map-marker.png';
-        
+
         // we define here the style of the map
         var style = [{
             "stylers": [{
@@ -35,11 +35,11 @@ $(function(){
             scrollwheel: false,
             styles: style
         };
-        
+
         // inizialize the map
         var map = new google.maps.Map(document.getElementById('google-container'), map_options);
-    
-        //add a custom marker to the map				
+
+        //add a custom marker to the map
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng($latitude, $longitude),
             map: map,
